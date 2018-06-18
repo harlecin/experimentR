068881046b38846a4048411836aebd2b18f0d321
Harlecin <christoph.bodner@gmail.com>
2018-06-18 20:51:16 +0200

    moved stuff to zzz

--- R/zzz-code-snippets.R
+++ R/zzz-code-snippets.R
@@ -45,6 +45,35 @@
 #   paste0("  (", rows, ")", collapse = ",\n")
 # ))
 
+# create experiment like so:
+#   experiment = experiment(
+#     id = "your id",
+#     auth = "your authorization token",
+#   )
+#   see: https://github.com/RevolutionAnalytics/azureml
+#
+#
+#   https://github.com/r-lib/R6/issues/42:
+#     R6list <- R6Class(
+#       "R6list",
+#       public = list(
+#         orig = NULL,
+#         initialize = function(x) {
+#           self$orig <- x
+#         },
+#         as.list = function() {
+#           self$orig
+#         }
+#       )
+#     )
+#
+#   as.list.R6list <- function(x, ...) {
+#     x$as.list()
+#   }
+#
+#   similar stuff for python:
+#     - https://github.com/mitdbg/modeldb/blob/master/server/codegen/sqlite/createDb.sql
+
 # TODO
 # -print() method to show runs
 # -summary() method to show overview
